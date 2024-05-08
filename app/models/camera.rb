63e008d4117da9061ac6f5c2a9075ab3d4658be5
Tony Nour <tonyanour1@gmail.com>
2024-05-08 13:09:30 +0100

    Changes to home page, seed file and controller

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,6 +1,6 @@
 class Camera < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependant: :destroy
-  validates :title, :description, :price, :booking, presence: true
+  has_many :bookings, dependent: :destroy
+  validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
 end
