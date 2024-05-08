e3ad699a26b0e98e501a849640ff44bf22fb2b2f
Joshua Sexton <joshua.peter.sexton+6RIW@gmail.com>
2024-05-08 14:49:50 +0100

    merging, implemented cloudinary, use Tony index file

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -24,3 +24,6 @@ class CamerasController < ApplicationController
   end
 
 end
+
+# upload image code for cloudinary (controller): result = Cloudinary::Uploader.upload("path/to/your/image.jpg")
+# display image code for cloudinary (page): <%= cl_image_tag("your_image_public_id", width: 400, height: 300, crop: :fill) %>
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dopxbkgda'
+  config.api_key = '627335589381199'
+  config.api_secret = 'UMaahdoVP6H0WN-OQ89aQSycfdc'
+  config.secure = true # Optional: ensure secure URLs
+end
