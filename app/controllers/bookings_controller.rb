fc83708ae9cbf7085d032c6cd288562feb0e4366
Tony Nour <tonyanour1@gmail.com>
2024-05-10 13:19:41 +0100

    merging master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    # @camera.user_id = current_user.id
+    @booking.save!
+    # redirect_to camera_path(@camera)
+  end
+
+
+  def camera_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
